a7efc260f493824720434d16a946ea2aaebb1230
Your Name <domtuz@yahoo.com>
2024-01-24 14:31:18 -0500

    Completed Interactive Map of USA

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const testRoute = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log('data:', data);
+}
+const url1 = 'https://datausa.io/api/data?drilldowns=State&measures=Population';
+const url2 = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
 
-setupCounter(document.querySelector('#counter'))
+testRoute(url1);
+testRoute(url2);
+
+const tooltipSpan = document.getElementById('details-box');
+
+document.addEventListener('mouseover', (e) => {
+    if (e.target.tagName == 'path') {
+        const content = e.target.dataset.name;
+        document.getElementById("details-box").innerHTML = content;
+        document.getElementById("details-box").style.opacity = "100%";
+    }
+    else {
+        document.getElementById("details-box").style.opacity = "0%";
+    }
+});
+
+window.onmousemove = function (e) {
+    let x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = (y + 20) + 'px';
+    tooltipSpan.style.left = (x) + 'px';
+};
+
+
+
