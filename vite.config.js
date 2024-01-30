2c4ad41f9cb86404f77bbb89b050e3cefe742b20
Your Name <domtuz@yahoo.com>
2024-01-30 11:12:29 -0500

    new build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
+  // build: { outDir: '..' },
   // needed for github pages just put the repo name here
   base: '/usPopulation-tracker/', 
 });
